17a850d328ba51f919e21a2c11e1bd63706ac300
Emi Brown <EmiCB@users.noreply.github.com>
2016-07-28 08:15:33 -0500

    Update 0.2.1
    + Item unloacalized name

--- src/main/java/com/emicb/themod/item/ItemCoffee.java
+++ src/main/java/com/emicb/themod/item/ItemCoffee.java
@@ -1,9 +1,14 @@
 package com.emicb.themod.item;
 
+import com.emicb.themod.reference.Reference;
+
+
 public class ItemCoffee extends ItemTM
 {
 	public ItemCoffee()
 	{
+		super();
 		
+		this.setUnlocalizedName("coffee");
 	}
 }
--- src/main/java/com/emicb/themod/item/ItemTM.java
+++ src/main/java/com/emicb/themod/item/ItemTM.java
@@ -1,11 +1,31 @@
 package com.emicb.themod.item;
 
+import com.emicb.themod.reference.Reference;
+
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemTM extends Item
 {
 	public ItemTM()
 	{
 		super();
+	}
+	
+	@Override
+	public String getUnlocalizedName()
+	{
+		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack)
+	{
+		return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+	}
+	
+	protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+	{
+		return unlocalizedName.substring(unlocalizedName.indexOf(".")+1);
 	}
 }
